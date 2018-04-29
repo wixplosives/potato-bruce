405d30c784b1da3200b4c4eb5704aaf4a741e54d
Tomer Lichtash <tomer@konzepz.com>
2018-04-29 11:40:25 +0300

    emoji top bar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,32 +2,34 @@ import * as React from 'react';
 import style from './../app.st.css';
 import { NavBarProps } from '../types';
 
+//   ⏄⏁⏇⏃
+// 
 const NavBar = (props: NavBarProps) => {
   const {
     handleMoodChange,
     handleSizeChange,
     handleBackdropChange,
-    resetAll
+    resetAll,
   } = props;
 
   return <div {...style('root')}>
     <div {...style('nav')}>
       <div {...style('buttonGroup header')}>
-        <button {...style('resetAllButton')} onClick={resetAll}>Reset</button>
-        <button value="perspective" onClick={handleBackdropChange}>Grid</button>
+        <button {...style('resetAllButton')} onClick={resetAll}>✖</button>
+        {/* <button value="perspective" onClick={handleBackdropChange}>👁</button> */}
       </div>
       <div {...style('buttonGroup mood')}>
-        <button value="happy" onClick={handleMoodChange}>Happy</button>
-        <button value="sad" onClick={handleMoodChange}>Sad</button>
-        <button value="glasses" onClick={handleMoodChange}>Glasses</button>
+        <button value="happy" onClick={handleMoodChange}>😀</button>
+        <button value="sad" onClick={handleMoodChange}>☺️</button>
+        <button value="glasses" onClick={handleMoodChange}>🕶</button>
       </div>
       <div {...style('buttonGroup size')}>
-        <button value="small" onClick={handleSizeChange}>Small</button>
-        <button value="big" onClick={handleSizeChange}>Big</button>
+        <button value="small" onClick={handleSizeChange}>-</button>
+        <button value="big" onClick={handleSizeChange}>+</button>
       </div>
       <div {...style('buttonGroup location')}>
-        <button value="paris" onClick={handleBackdropChange}>Paris</button>
-        <button value="wixhq" onClick={handleBackdropChange}>WixHQ</button>
+        <button value="paris" onClick={handleBackdropChange}>🇫🇷</button>
+        <button value="wixhq" onClick={handleBackdropChange}>🏙</button>
       </div>
     </div>
 
