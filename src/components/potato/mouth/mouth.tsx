c823c43720f15375f390fb27aa8f748c9886aaee
tomr <tomr@wix.com>
2019-03-13 13:21:34 +0200

    fix style inheritence in mouth component

--- src/components/potato/mouth/mouth.tsx
+++ src/components/potato/mouth/mouth.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { style, classes } from './mouth.st.css';
 
 export interface MouthProps {
-    className?: string;
+    className: string;
 }
 
 export const Mouth: React.SFC<MouthProps> = (props: MouthProps) => (
-    <div className={style(classes.root, classes.props)} >
+    <div className={style(classes.root, props.className)} >
         <div className={classes.upperLip} />
         <div className={classes.tongue} />  
         <div className={classes.lowerLip} />
