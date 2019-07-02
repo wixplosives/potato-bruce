2f0a9f575ac0dbdf10e4362b794e1e9b3726ccd9
tomr <tomr@wix.com>
2019-07-02 16:00:21 +0300

    Updates

---
+++ precache-manifest.af0b6e0242ca533541baee8baee3c250.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf33c656797f785c175c30732722ead2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3f17a2fdfa2bf665412",
+    "url": "/main.bundle.css"
+  },
+  {
+    "revision": "f3f17a2fdfa2bf665412",
+    "url": "/static/js/main.f3f17a2f.js"
+  },
+  {
+    "revision": "616b69dd1b54e81cd99d997f0192b438",
+    "url": "/static/media/amsterdam.616b69dd.jpg"
+  },
+  {
+    "revision": "9ac279f97fd30bde7b19709196d0a23c",
+    "url": "/static/media/github-logo.9ac279f9.png"
+  },
+  {
+    "revision": "f898d36716eb18af688aeae03666c6e1",
+    "url": "/static/media/glasses.f898d367.png"
+  },
+  {
+    "revision": "871df20e8c952011684a918c10ac0178",
+    "url": "/static/media/mohawk.871df20e.svg"
+  },
+  {
+    "revision": "9aa0a42d3893e62f64d6fed789365384",
+    "url": "/static/media/paris.9aa0a42d.jpg"
+  },
+  {
+    "revision": "f1213de5be5626caddcd9a3d8f1d0bf7",
+    "url": "/static/media/potato-small.f1213de5.png"
+  },
+  {
+    "revision": "a2b412b5796b5fe311d11670d5577f75",
+    "url": "/static/media/stylable-logo.a2b412b5.png"
+  },
+  {
+    "revision": "0e636f07a075f5218fb11635a038d4ca",
+    "url": "/static/media/wixhq.0e636f07.jpg"
+  }
+]);
